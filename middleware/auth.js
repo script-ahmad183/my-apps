fa94eddaa694d4725ac1c0943a94b4ac8582a5d6
FO UMUM <ahmadrevi98@gmail.com>
2020-09-25 14:53:27 +0700

    API Register dan Login

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ var response = require('../res');
 var jwt = require('jsonwebtoken');
 var config = require('../config/secret');
 var ip = require('ip');
-const e = require('express');
+const express = require('express'); 
 
 //registrasi
 exports.registrasi = function(req, res){
@@ -13,16 +13,16 @@ exports.registrasi = function(req, res){
         username : req.body.username,
         email : req.body.email,
         nama : req.body.nama,
-        password : req.body.password,
+        password : md5(req.body.password),
         level : req.body.level
     }
 
-    var query = "SELECT email FROM ?? WHERE email=??";
+    var query = "SELECT email FROM ?? WHERE ??=?";
     var table = ["user","email", post.email];
 
     query = mysql.format(query,table);
 
-    connection.query(query, function(error,rows){
+    connection.query(query, function(error, rows){
         if(error){
             console.log(error);
         }else{
@@ -42,4 +42,54 @@ exports.registrasi = function(req, res){
             }
         }
     })
+}
+
+// login
+exports.login = function(req, res){
+    var post = {
+        password : md5(req.body.password),
+        email : req.body.email
+    }
+
+    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+    var table = ["user", "password", post.password, "email", post.email];
+
+    query = mysql.format(query, table);
+    connection.query(query, function(error, rows){
+        if(error){
+            console.log(error);
+        }else{
+           if(rows.length==1){
+               var token = jwt.sign({rows}, config.secret, {
+                   expiresIn:1440
+               });
+               id_user = rows[0].id_user;
+
+               var data = {
+                   id_user : id_user,
+                   access_token : token,
+                   ip : ip.address()
+               }
+
+               var query = "INSERT INTO ?? SET ?";
+               var table = ['akses_token'];
+
+               query = mysql.format(query, table);
+               connection.query(query, data, function(error, rows){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        res.json({
+                            success:true,
+                            message:'Token JWT tergenerate!',
+                            token:token,
+                            currUser:data.id_user
+                        });
+                    }
+               });
+           }else{
+               res.json({"Error":true, "Message":"Email atau password salah!"});
+           }
+        }
+    });
 }
--- middleware/index.js
+++ middleware/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var auth = require('./auth');
 var router = express.Router();
 
-//registrasi
+
 router.post('/api/v1/register', auth.registrasi);
+router.post('/api/v1/login', auth.login);
 
 module.exports = router;
